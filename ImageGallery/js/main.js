1cfa55eb78dcfde2ce4ce1dbbfe5e15e367ac1ce
PRIYA-PC\PRIYA <priyanka.karolia17@gmail.com>
2017-07-18 23:45:12 +0530

    update

--- ImageGallery/js/main.js
+++ ImageGallery/js/main.js
@@ -1,7 +1,15 @@
 function app(){
-	this.imgArray= ["./images/img1.JPG", "./images/img2.JPG"];
+	this.imgArray= ["./images/img1.JPG", "./images/img2.JPG", "./images/img3.JPG", "./images/img4.JPG"];
 
 	this.sliderWrapper= document.getElementById("sliderWrapper");
+
+	this.activeIndex= 0;
+
+	this.prevButton= document.getElementsByClassName("previousBtn")[0];
+
+	this.nextButton= document.getElementsByClassName("nextBtn")[0];
+
+	this.getSliderPosition();
 
 	this.renderGallery();
 }
@@ -10,18 +18,118 @@ app.prototype= {
 	constructor: app,
 
 	renderGallery: function(){
+		var width= 0;
 		for(let i=0; i< this.imgArray.length; i++){
-			this.sliderWrapper.appendChild(this.getImageElement(this.imgArray[i])); 
+			width+= 1000;
+			this.sliderWrapper.appendChild(this.getImageElement(this.imgArray[i], i)); 
 		}
+		this.sliderWrapper.style.width= width + "px";
+		this.updateGallery();
 	},
 
-	getImageElement: function(imgUrl){
+	getSliderPosition: function(){
+		var sliderPosition= document.getElementsByClassName("parentWrapper")[0].getBoundingClientRect();
+		this.sliderMidPosition= (sliderPosition.left+ sliderPosition.right)/2
+	},
+
+	updateGallery: function(){
+		this.handleButtons();
+
+		this.renderActiveImage();
+	},
+
+	handleButtons: function(){
+		if(this.activeIndex === 0){
+			this.prevButton.classList.add("disabled");
+		}
+		else{
+			this.prevButton.classList.remove("disabled");
+		}
+		if(this.activeIndex === (this.imgArray.length - 1)){
+			this.nextButton.classList.add("disabled");
+		}
+		else{
+			this.nextButton.classList.remove("disabled");
+		}
+	},
+	
+	renderActiveImage: function(){
+		for(let i=0; i< this.imgArray.length; i++){
+			var element= document.getElementById(i);
+			element.classList.remove("active");
+
+			if(this.activeIndex === i){
+				element.classList.add("active");
+			}
+		}
+		var selectedImg= document.getElementById(this.activeIndex);
+		selectedImg.classList.add("active");
+	},
+
+	getImageElement: function(imgUrl, index){
 		var span= document.createElement("span");
+		span.setAttribute("id", index);
 		var img= document.createElement("img");
 		img.src= imgUrl;
 		span.appendChild(img);
 		return span;
-	}
+	},
+
+	handlePreviousBtn:function(){
+		this.activeIndex--;
+		this.updateGallery();
+	},
+
+	handleNextBtn: function(){
+		this.activeIndex++;
+		this.updateGallery();
+	},
+
+	handleSliderMouseDown: function(e){
+		e.preventDefault();
+		var targetElement= e.target;
+		if(e.clientX > this.sliderMidPosition){
+			this.direction="rightToLeft";		
+		}
+		else if(e.clientX < this.sliderMidPosition){
+			this.direction="leftToRight";
+		}
+		document.addEventListener("mouseup", function(e){
+			targetElement.removeEventListener("mousemove");
+		});
+		targetElement.addEventListener("mousemove", function(e){
+			e.preventDefault();
+			if(e.clientX > this.sliderMidPosition-50 && e.clientX < this.sliderMidPosition+50){
+				if(this.direction === "rightToLeft"){
+					document.getElementById(this.activeIndex+1).classList.add("nextActive");
+					document.getElementById(this.activeIndex).classList.add("prevActive");
+				}
+				else if(this.direction === "leftToRight"){
+					document.getElementById(this.activeIndex-1).classList.add("prevActive");
+					document.getElementById(this.activeIndex).classList.add("nextActive");
+				}
+			}
+			else{
+				if(e.clientX < this.sliderMidPosition-50 && this.direction === "rightToLeft"){
+					this.activeIndex+= 1;
+					document.getElementById(this.activeIndex-1).classList.remove("prevActive");
+					document.getElementById(this.activeIndex).classList.remove("nextActive");
+					document.getElementById(this.activeIndex-1).classList.remove("active");
+					document.getElementById(this.activeIndex).classList.add("active");
+				}
+				else if(e.clientX > this.sliderMidPosition+50 && this.direction === "leftToRight"){
+					this.activeIndex-= 1;
+					document.getElementById(this.activeIndex).classList.remove("prevActive");
+					document.getElementById(this.activeIndex+1).classList.remove("nextActive");
+					document.getElementById(this.activeIndex+1).classList.remove("active");
+					document.getElementById(this.activeIndex).classList.add("active");
+				}
+				this.handleButtons();
+			}
+
+		}.bind(this));
+	},
+
 }
 
 var myApp= new app();
